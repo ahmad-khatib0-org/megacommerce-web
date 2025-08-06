cd88350a99ac212ba1be7eb423f61e4cfae7f5f5
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-08-06 23:29:27 +0300

    scaffold shared stepper

--- packages/shared/src/models/app/index.ts
+++ packages/shared/src/models/app/index.ts
@@ -1,2 +1,16 @@
 export * from './context'
 export * from './trans'
+
+/// Toast
+export type ToastPosition = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
+export type ToastType = 'success' | 'warning' | 'error'
+
+/**
+ * @field time in milliseconds
+ */
+export interface ToastItem {
+  id: string
+  message: string
+  type: ToastType
+  time?: number
+}
---
+++ packages/ui/src/lib/client/stepper.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { useState } from 'react'
+import { Button } from '@mantine/core'
+import { toast } from 'react-toastify'
+
+type Props = {
+  steps: React.ReactNode[]
+  labels: string[]
+  clickNext: string
+  nextMsg: string
+  prevMsg: string
+}
+
+export function Stepper({ labels, steps, clickNext, nextMsg, prevMsg }: Props) {
+  const [current, setCurrent] = useState(0)
+
+  const onStepClick = async (idx: number) => {
+    if (idx > current) toast(clickNext)
+    setCurrent(idx)
+  }
+
+  return (
+    <div className="flex flex-col items-center *:w-full w-max">
+      <ul className="flex items-center gap-8 mb-4">
+        {labels.map((step, idx) => {
+          const isCompleted = idx < current
+          const isCurrent = idx === current
+          const isUpcoming = idx > current
+
+          return (
+            <li
+              key={step}
+              className="flex flex-col items-center cursor-pointer"
+              onClick={() => onStepClick(idx)}>
+              <div
+                className={`w-12 h-12 flex items-center justify-center rounded-full text-xl font-medium mb-1
+                  ${isCompleted ? 'bg-orange-500 text-white' : ''}
+                  ${isCurrent ? 'bg-white border-[5px] border-orange-500 text-orange-500' : ''}
+                  ${isUpcoming ? 'border border-orange-300 text-orange-300' : ''}
+                `}>
+                {idx + 1}
+              </div>
+              <span className="text-xl text-gray-600">{step}</span>
+            </li>
+          )
+        })}
+      </ul>
+      <div className="mt-4">{steps[current]}</div>
+      <div className="flex justify-between items-center">
+        <Button title={prevMsg}>{prevMsg}</Button>
+        <Button title={nextMsg} gradient={{ from: 'blue', to: 'cyan', deg: 90 }} variant="gradient">
+          {nextMsg}
+        </Button>
+      </div>
+    </div>
+  )
+}
