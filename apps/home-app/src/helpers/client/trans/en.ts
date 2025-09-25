515c8b7a525f014e89da2447a55e637da3cdc162
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-09-25 18:00:51 +0300

    init client side translations

---
+++ apps/home-app/src/helpers/client/trans/en.ts
@@ -0,0 +1,11 @@
+import 'client-only'
+import { Translations } from ".";
+
+export const TransEN: Translations = {
+  "search.looking_for": "",
+  "location.ship_to": "",
+  "translation.website_language": "",
+  "translation.language": "",
+  "currencies.currency": "",
+  save: ""
+}
---
+++ apps/home-app/src/helpers/client/trans/index.ts
@@ -0,0 +1,11 @@
+import 'client-only'
+
+export interface Translations {
+  "search.looking_for": string
+  "location.ship_to": string,
+  "translation.website_language": string,
+  "translation.language": string,
+  "currencies.currency": string,
+  save: string,
+}
+
