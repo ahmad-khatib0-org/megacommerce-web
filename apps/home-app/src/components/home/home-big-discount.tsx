d67ca1789f17a2b2074a59a17c8e67916ef32d5e
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-10-04 12:10:39 +0300

    add HomeProductsWrapper, HomeBigDiscount

---
+++ apps/home-app/src/components/home/home-big-discount.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { ObjString } from "@megacommerce/shared"
+import HomeProductsWrapper from '@/components/home/home-products-wrapper'
+
+type Props = {
+  tr: ObjString
+}
+
+function HomeBigDiscount({ tr }: Props) {
+  const products = new Array(6).fill(null).map((_, i) => ({
+    id: `product-${i}`,
+    name: 'the product name and this is ...',
+    image: '/images/login.png',
+    body: <>
+      <div className='flex items-center gap-x-2'>
+        <p className='font-bold text-base'>40.44$</p>
+        <p className='line-through text-sm'>44.44$</p>
+      </div>
+      <div className='bg-red-500 px-2 py-1 mt-1 w-max'>
+        <p className='text-white text-sm'>-6%</p>
+      </div>
+    </>
+  }))
+
+  return <HomeProductsWrapper title={tr.bigDisc} products={products} />
+}
+
+export default HomeBigDiscount
---
+++ apps/home-app/src/components/home/home-products-wrapper.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { ReactNode, useCallback } from "react"
+import Image from "next/image"
+import useEmblaCarousel from 'embla-carousel-react'
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react"
+
+interface Product {
+  id: string
+  name: string
+  image: string
+  body: ReactNode
+}
+
+type Props = {
+  title: string
+  products: Product[]
+}
+
+function HomeProductsWrapper({ title, products }: Props) {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ dragFree: false, slidesToScroll: 2, loop: true }, [])
+
+  const prev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const next = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
+  return (
+    <div className='embla relative group/item border border-black/20 hover:shadow-md hover:bg-slate-100'>
+      <p className="text-center font-semibold my-4 text-2xl">{title}</p>
+      <div ref={emblaRef} className='embla__viewport overflow-hidden mb-4 px-4'>
+        <div className='embla__container flex gap-x-3'>
+          {products.map((p, idx) => (
+            <div
+              key={`${p.id}${idx}`}
+              style={{ flex: '0 0 50%' }}
+              className='embla__slide flex flex-col'>
+              <div className="relative h-40 w-full">
+                <Image src={p.image} alt={p.name} fill className="object-cover" />
+              </div>
+              <p className="line-clamp-2 text-center mb-4">{p.name}</p>
+              {p.body}
+            </div>
+          ))}
+        </div>
+      </div>
+      <button onClick={() => prev()} className="absolute top-1/2 -translate-y-1/2 invisible group-hover/item:visible bg-slate-500 p-2">
+        <IconArrowLeft color="#fbfbfb" />
+      </button>
+      <button onClick={() => next()} className="absolute top-1/2 -translate-y-1/2 right-0 invisible group-hover/item:visible bg-slate-500 p-2">
+        <IconArrowRight color="#fbfbfb" />
+      </button>
+    </div>
+  )
+}
+
+export default HomeProductsWrapper
